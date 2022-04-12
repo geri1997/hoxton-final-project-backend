de645a7b7b919df5f228b8e33c78d9b4592f3813
Geri <luga.geri@gmail.com>
2022-04-12 19:25:36 +0200

    Added genres in seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,84 @@
-import { PrismaClient } from '@prisma/client'
-import bcrypt from 'bcryptjs'
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error']
-})
+    log: ['query', 'info', 'warn', 'error'],
+});
 
 const users = [
-  {
-    id: 1,
-    userName:  'avenger22',
-    email: 'jurgenhasmeta@email.com',
-    password: bcrypt.hashSync("jurgen123", 8)
-  },
-  {
-    id: 2,
-    userName:  'geri12',
-    email: 'geri@email.com',
-    password: bcrypt.hashSync("geri123", 8)  
-  },
-  {
-    id: 3,
-    userName:  'visard12',
-    email: 'andrea@email.com',
-    password: bcrypt.hashSync("visard123", 8)  
-  },
-  {
-    id: 4,
-    userName:  'marsel12',
-    email: 'marsel@email.com',
-    password: bcrypt.hashSync("marsel123", 8)  
-  }
-]
+    {
+        id: 1,
+        userName: 'avenger22',
+        email: 'jurgenhasmeta@email.com',
+        password: bcrypt.hashSync('jurgen123', 8),
+    },
+    {
+        id: 2,
+        userName: 'geri12',
+        email: 'geri@email.com',
+        password: bcrypt.hashSync('geri123', 8),
+    },
+    {
+        id: 3,
+        userName: 'visard12',
+        email: 'andrea@email.com',
+        password: bcrypt.hashSync('visard123', 8),
+    },
+    {
+        id: 4,
+        userName: 'marsel12',
+        email: 'marsel@email.com',
+        password: bcrypt.hashSync('marsel123', 8),
+    },
+];
 
-async function createStuff () {
+const genres = [
+    { name: 'Aksion' },
+    { name: 'Aziatik' },
+    { name: 'Krim' },
+    { name: 'NETFLIX' },
+    { name: 'Horror' },
+    { name: 'Thriller' },
+    { name: 'Fantashkencë' },
+    { name: 'Fantazi' },
+    { name: 'Dramë' },
+    { name: 'Spanjoll' },
+    { name: 'Aventurë' },
+    { name: 'Komedi' },
+    { name: 'Histori' },
+    { name: 'Luftë' },
+    { name: 'Rus' },
+    { name: 'Romancë' },
+    { name: 'Biografi' },
+    { name: 'Italian' },
+    { name: 'Mister' },
+    { name: 'Sport' },
+    { name: 'Familjar' },
+    { name: 'Muzikë' },
+    { name: 'Animuar' },
+    { name: 'Erotik +18' },
+    { name: 'Nordik' },
+    { name: 'Dokumentar' },
+    { name: 'Turk' },
+    { name: 'Gjerman' },
+    { name: 'Francez' },
+    { name: 'Hindi' },
+];
 
-    //@ts-ignore
-    await prisma.user.deleteMany()
+async function createStuff() {
   
+    await prisma.user.deleteMany();
+
     for (const user of users) {
-      await prisma.user.create({ data: user })
+        await prisma.user.create({ data: user });
     }
-  
+
+
+    await prisma.genre.deleteMany();
+
+    for (const genre of genres) {
+        await prisma.genre.create({ data: genre });
+    }
 }
-  
-createStuff()
+
+createStuff();
